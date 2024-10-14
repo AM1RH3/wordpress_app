546df666db5c0124ec172fbf09a71decce983a0c
AM1RH3 <amirhossein.rasooli@outlook.com>
2024-10-14 17:28:46 +0330

    Payment O. part2 Ui

--- lib/ui/payment_options/payment_options.dart
+++ lib/ui/payment_options/payment_options.dart
@@ -3,6 +3,8 @@ import 'package:intl/intl.dart' show NumberFormat;
 import 'package:provider/provider.dart';
 import 'package:woedpress_app/constants/constants.dart';
 import 'package:woedpress_app/providers/shop_provider.dart';
+import 'package:woedpress_app/ui/payment_options/payment_utils.dart';
+import 'package:woedpress_app/ui/utils/extensions.dart';
 
 class PaymentOptions extends StatefulWidget {
   const PaymentOptions({super.key});
@@ -67,20 +69,108 @@ class _PaymentOptionsState extends State<PaymentOptions> {
                   color: Constants.primaryColor.withOpacity(0.3),
                 ),
                 child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    SizedBox(
-                      height: 20.0,
-                      child: Image.asset('assets/images/PriceUnit-green.png'),
+                    Row(
+                      children: [
+                        SizedBox(
+                          height: 20.0,
+                          child:
+                              Image.asset('assets/images/PriceUnit-green.png'),
+                        ),
+                        const SizedBox(width: 5.0),
+                        Text(
+                          numberFormat
+                              .format(cartModel.totalAmount)
+                              .farsiNumber,
+                          style: TextStyle(
+                            fontFamily: 'Lalezar',
+                            color: Constants.primaryColor,
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                      ],
                     ),
-                    const SizedBox(width: 5.0),
-                    Text(),
+                    const Text(
+                      textDirection: TextDirection.rtl,
+                      ': مبلغ نهایی ',
+                      style: TextStyle(
+                        fontFamily: 'Lalezar',
+                        fontSize: 25.0,
+                      ),
+                    ),
                   ],
                 ),
               ),
             ),
+            body: _formUI(),
           );
         }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
       },
     );
   }
+}
+
+Widget _formUI() {
+  return SingleChildScrollView(
+    child: Directionality(
+      textDirection: TextDirection.rtl,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: Column(
+          children: [
+            //! ONLINE PAYMENTS
+
+            const BuildPamentOptions(
+              icon: Icons.payment,
+              paymentTitle: 'پرداخت آنلاین',
+              paymentDescription: 'از روش های زیر یکی را انتخاب کنید',
+            ),
+            // Payment Methods
+            const SizedBox(height: 20.0),
+
+            // ZarinPal
+            BuildClickPaymentMethod(
+              paymentTitle: 'زرین پال',
+              paymentDescription: 'پرداخت آنلاین با درگاه زرین پال',
+              assetImageUrl: 'assets/images/zarin.png',
+              onPressed: () {},
+            ),
+            const SizedBox(height: 15.0),
+            // NextPay
+            BuildClickPaymentMethod(
+              paymentTitle: 'زرین پال',
+              paymentDescription: 'پرداخت آنلاین با درگاه زرین پال',
+              assetImageUrl: 'assets/images/zarin.png',
+              onPressed: () {},
+            ),
+            const SizedBox(height: 50.0),
+
+            //! OFFLINE PAYMENTS
+
+            const BuildPamentOptions(
+              icon: Icons.location_on,
+              paymentTitle: 'سایر روش های پرداخت',
+              paymentDescription: 'از روش های زیر یکی را انتخاب کنید',
+            ),
+            // Payment Methods
+            const SizedBox(height: 20.0),
+
+            // ZarinPal
+            BuildClickPaymentMethod(
+              paymentTitle: 'پرداخت در محل',
+              paymentDescription: 'پرداخت درب منزل با دستگاه کارتخوان',
+              assetImageUrl: 'assets/images/cod.png',
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
 }
---
+++ lib/ui/payment_options/payment_utils.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:woedpress_app/constants/constants.dart';
+
+class BuildPamentOptions extends StatelessWidget {
+  final String paymentTitle;
+  final String paymentDescription;
+  final IconData icon;
+  const BuildPamentOptions({
+    required this.paymentTitle,
+    required this.paymentDescription,
+    required this.icon,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: ListTile(
+        contentPadding: const EdgeInsets.symmetric(vertical: 0.0),
+        leading: Icon(
+          icon,
+          color: Constants.primaryColor,
+          size: 45.0,
+        ),
+        title: Text(
+          paymentTitle,
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(
+            fontFamily: 'Lalezar',
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        subtitle: Text(
+          paymentDescription,
+          style: const TextStyle(
+            fontFamily: 'Lalezar',
+            fontWeight: FontWeight.bold,
+            fontSize: 14.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BuildClickPaymentMethod extends StatelessWidget {
+  final String assetImageUrl;
+  final String paymentTitle;
+  final String paymentDescription;
+  final VoidCallback onPressed;
+  const BuildClickPaymentMethod({
+    required this.assetImageUrl,
+    required this.paymentTitle,
+    required this.paymentDescription,
+    required this.onPressed,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        decoration: BoxDecoration(
+          color: Constants.primaryColor.withOpacity(0.1),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              height: 45.0,
+              width: 40.0,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(5.0),
+                ),
+                image: DecorationImage(
+                  image: AssetImage(assetImageUrl),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(width: 15.0),
+            Flexible(
+                child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        paymentTitle,
+                        style: const TextStyle(
+                          fontFamily: 'iranSans',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                      Text(
+                        paymentDescription,
+                        style: const TextStyle(
+                          fontFamily: 'iranSans',
+                          fontSize: 14.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
