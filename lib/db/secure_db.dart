77440b4025f9a57e68c06f8ca3daca338a17f369
AM1RH3 <amirhossein.rasooli@outlook.com>
2024-10-31 14:24:41 +0330

    add secure_db

---
+++ lib/db/secure_db.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:woedpress_app/models/woocommerce/login_model.dart';
+
+class SecureSorageDB {
+  final FlutterSecureStorage storage = const FlutterSecureStorage(
+    aOptions: AndroidOptions(
+      encryptedSharedPreferences: true,
+    ),
+  );
+
+  Future setLoginDetails(
+    LoginResponseModel? loginResponseModel,
+  ) async {
+    return await storage.write(
+      key: 'login_details',
+      value: loginResponseModel.toString().isNotEmpty
+          ? jsonEncode(
+              loginResponseModel!.toJson(),
+            )
+          : '',
+    );
+  }
+
+  Future loginDetails() async {
+    String loginDetails = await storage.read(key: 'login_details') as String;
+    if (loginDetails.isNotEmpty) {
+      LoginResponseModel data = LoginResponseModel.fromJson(
+        jsonDecode(
+          loginDetails,
+        ),
+      );
+      return data;
+    }
+  }
+
+  Future<bool> isLoggedin() async {
+    String? value = await storage.read(key: 'login_details');
+    return value != null ? true : false;
+  }
+
+  Future<void> logOut() async {
+    await storage.delete(key: 'login_details');
+  }
+}
