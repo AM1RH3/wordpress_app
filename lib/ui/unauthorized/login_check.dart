4a715b7a889be25b62864206ec38e55242aea475
AM1RH3 <amirhossein.rasooli@outlook.com>
2024-10-31 14:31:23 +0330

    build LoginORNot

---
+++ lib/ui/unauthorized/login_check.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:woedpress_app/db/shared_pr_db.dart';
+import 'package:woedpress_app/ui/login/login_page.dart';
+import 'package:woedpress_app/ui/root/root_page.dart';
+
+class LoginOrNot extends StatelessWidget {
+  const LoginOrNot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: SharedService.isLoggedin(),
+      builder: (BuildContext context, AsyncSnapshot<bool> loginModel) {
+        if (loginModel.hasData && loginModel.data!) {
+          return const RootPage();
+        } else {
+          return const LoginPage();
+        }
+      },
+    );
+  }
+}
